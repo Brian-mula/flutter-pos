4222edc2e6dd7ca432e31e614b7122a9ef842c14
Brian-mula <mulatiwangilabrian@gmail.com>
2022-09-13 00:23:26 +0300

    implemented the login page

--- lib/views/auth/login_page.dart
+++ lib/views/auth/login_page.dart
@@ -1,46 +1,85 @@
-import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pos/widgets/cutome_text.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-class LoginPage extends ConsumerWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController controller = TextEditingController();
+  String initialCountry = 'KE';
+  PhoneNumber number = PhoneNumber(isoCode: 'KE');
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const CustomeText(
-          text: "Flutter POS",
-          size: 18,
-        ),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-        child: Stack(
-          children: [
-            Positioned(
-                left: 50,
-                right: 50,
-                top: 30,
-                child: Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      image: const DecorationImage(
-                          image: NetworkImage(
-                              'https://cdn.pixabay.com/photo/2018/01/13/10/29/business-3079910__340.jpg'),
-                          fit: BoxFit.cover)),
-                )),
-            Positioned(
-                top: 400,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [],
-                ))
-          ],
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              InternationalPhoneNumberInput(
+                onInputChanged: (PhoneNumber number) {
+                  print(number.phoneNumber);
+                },
+                onInputValidated: (bool value) {
+                  print(value);
+                },
+                selectorConfig: const SelectorConfig(
+                  selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                ),
+                ignoreBlank: false,
+                autoValidateMode: AutovalidateMode.disabled,
+                selectorTextStyle: const TextStyle(color: Colors.black),
+                initialValue: number,
+                textFieldController: controller,
+                formatInput: false,
+                keyboardType: const TextInputType.numberWithOptions(
+                    signed: true, decimal: true),
+                inputBorder: const OutlineInputBorder(),
+                onSaved: (PhoneNumber number) {
+                  print('On Saved: $number');
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blue.shade500),
+                    elevation: MaterialStateProperty.all(0.0)),
+                onPressed: () {
+                  formKey.currentState!.save();
+                },
+                child: const Text('Continue'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void getPhoneNumber(String phoneNumber) async {
+    PhoneNumber number =
+        await PhoneNumber.getRegionInfoFromPhoneNumber(phoneNumber, 'US');
+
+    setState(() {
+      this.number = number;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
