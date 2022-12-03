3752ef03633dca6f6a56765fb4b7b596f630c76f
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-04 02:10:31 +0300

    implemented product logic

--- lib/logic/products/products_logic.dart
+++ lib/logic/products/products_logic.dart
@@ -6,5 +6,29 @@ class ProductsLogic {
   final CollectionReference _products =
       FirebaseFirestore.instance.collection('products');
 
-  Future<void> addStock(ProductModel productModel) async {}
+// ! add new product
+  Future<void> addStock(ProductModel productModel) async {
+    _products.add(productModel.toSnapshot());
+  }
+
+  // ! get all products
+
+  Future<List<ProductModel>> getAllProduct() async {
+    QuerySnapshot<Map<String, dynamic>> snapshots =
+        await FirebaseFirestore.instance.collection("products").get();
+
+    return snapshots.docs.map((doc) => ProductModel.fromSnapshot(doc)).toList();
+  }
+
+  // ! delete a product
+
+  Future<void> deleteProduct(String id) async {
+    await _products.doc(id).delete();
+  }
+
+  // ! update a product
+
+  Future<void> updateProduct(String id, ProductModel productModel) async {
+    await _products.doc(id).update(productModel.toSnapshot());
+  }
 }
