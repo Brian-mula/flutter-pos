6cae3967adf27e9e938bce23fc5ab308b14a9ba7
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-03 11:42:01 +0300

    modified json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pos/Homepage.dart';
+import 'package:pos/firebase_options.dart';
 import 'package:pos/theme/custome_theme.dart';
 import 'package:pos/views/auth/verify.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
