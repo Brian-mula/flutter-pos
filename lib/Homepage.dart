8ed2c1e2f70c8cdbc3d0d896eff4881bebde8247
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-03 23:50:01 +0300

    implemented phone authentication

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:intl_phone_field/phone_number.dart';
 import 'package:pinput/pinput.dart';
 import 'package:pos/widgets/cutome_text.dart';
 
@@ -13,31 +15,59 @@ class HomePage extends ConsumerStatefulWidget {
 
 class _HomePageState extends ConsumerState<HomePage> {
   final TextEditingController phoneController = TextEditingController();
+  final TextEditingController pinController = TextEditingController();
+  String verificationId = '';
   bool isPhone = true;
+  bool isLoading = false;
+  String? countryCode;
 
-  Future<void> loginUser() async {
-    setState(() {
-      isPhone = false;
-    });
+  Future<void> signInWithPhoneAuth(PhoneAuthCredential credential) async {
+    try {
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      print(e.toString());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    return isPhone
-        ? Scaffold(
-            backgroundColor: Colors.white,
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              centerTitle: true,
-              elevation: 0.0,
-              title: CustomeText(
-                text: "Khetia Supermarket",
-                textStyle: theme.textTheme.headline5!
-                    .copyWith(color: Colors.blue.shade600),
-              ),
-            ),
-            body: Container(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0.0,
+        title: CustomeText(
+          text: "Khetia Supermarket",
+          textStyle:
+              theme.textTheme.headline5!.copyWith(color: Colors.blue.shade600),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue.shade700,
+          child: const Icon(
+            Icons.arrow_forward_ios,
+            size: 30,
+            color: Colors.white,
+          ),
+          onPressed: () async {
+            FirebaseAuth auth = FirebaseAuth.instance;
+            await auth.verifyPhoneNumber(
+              phoneNumber: countryCode! + phoneController.text,
+              verificationCompleted: (PhoneAuthCredential credential) {},
+              verificationFailed: (FirebaseAuthException e) {},
+              codeSent: (String verificationId, int? resendToken) {
+                setState(() {
+                  isPhone = false;
+                  this.verificationId = verificationId;
+                });
+              },
+              codeAutoRetrievalTimeout: (String verificationId) {},
+            );
+          }),
+      body: isPhone
+          ? Container(
               padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -63,45 +93,23 @@ class _HomePageState extends ConsumerState<HomePage> {
                       ),
                     ),
                     initialCountryCode: 'KE',
+                    onSaved: (PhoneNumber? phoneNumber) {
+                      print("phone is: $phoneNumber");
+                    },
                     onChanged: (phone) {
+                      setState(() {
+                        countryCode = phone.countryCode;
+                      });
                       print(phone.completeNumber);
                     },
                   ),
                   const SizedBox(
                     height: 20,
                   ),
-                  SizedBox(
-                    width: double.maxFinite,
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Colors.blue.shade500)),
-                        onPressed: () async {
-                          await loginUser();
-                        },
-                        child: CustomeText(
-                          text: "Continue",
-                          textStyle: theme.textTheme.headline6!
-                              .copyWith(color: Colors.white),
-                        )),
-                  )
                 ],
               ),
-            ),
-          )
-        : Scaffold(
-            backgroundColor: Colors.white,
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              centerTitle: true,
-              elevation: 0.0,
-              title: CustomeText(
-                text: "Verification screen",
-                textStyle: theme.textTheme.headline5!
-                    .copyWith(color: Colors.blue.shade600),
-              ),
-            ),
-            body: Container(
+            )
+          : Container(
               padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -112,28 +120,24 @@ class _HomePageState extends ConsumerState<HomePage> {
                   const SizedBox(
                     height: 20,
                   ),
-                  const Pinput(
+                  Pinput(
                     length: 6,
+                    controller: pinController,
+                    onSubmitted: (String pin) {
+                      isLoading = true;
+                      PhoneAuthCredential credential =
+                          PhoneAuthProvider.credential(
+                              verificationId: verificationId,
+                              smsCode: pinController.text);
+                      signInWithPhoneAuth(credential);
+                    },
                   ),
                   const SizedBox(
                     height: 10,
                   ),
-                  SizedBox(
-                    width: double.maxFinite,
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Colors.blue.shade500)),
-                        onPressed: () {},
-                        child: CustomeText(
-                          text: "Verify",
-                          textStyle: theme.textTheme.headline6!
-                              .copyWith(color: Colors.white),
-                        )),
-                  )
                 ],
               ),
             ),
-          );
+    );
   }
 }
