ef71ea35ba4fc65886cf3a82e56d91356e9b9301
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-07 08:27:35 +0300

    implemented product upload

--- lib/views/new_product.dart
+++ lib/views/new_product.dart
@@ -1,5 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
 import 'package:pos/widgets/cutome_text.dart';
 
 class NewProduct extends ConsumerStatefulWidget {
@@ -11,8 +18,26 @@ class NewProduct extends ConsumerStatefulWidget {
 
 class _NewProductState extends ConsumerState<NewProduct> {
   final _formKey = GlobalKey<FormState>();
+  FirebaseStorage storage = FirebaseStorage.instance;
   final TextEditingController nameController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
+  String productCode = "";
+  String imageUlr = "";
+
+  Future<void> pickImage() async {
+    final picker = ImagePicker();
+    XFile? pickedImage;
+    try {
+      pickedImage = await picker.pickImage(source: ImageSource.gallery);
+      final String fileName = path.basename(pickedImage!.name);
+      File file = File(pickedImage.path);
+      var snapshot = storage.ref().child("images/$fileName");
+      final downloadUrl = await snapshot.getDownloadURL();
+      setState(() {
+        imageUlr = downloadUrl;
+      });
+    } catch (e) {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,6 +98,61 @@ class _NewProductState extends ConsumerState<NewProduct> {
                           borderRadius: BorderRadius.circular(6),
                           borderSide: BorderSide(color: Colors.blue.shade600))),
                 ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton.icon(
+                    style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(0.0),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blue.shade600)),
+                    onPressed: () async {
+                      productCode = await FlutterBarcodeScanner.scanBarcode(
+                          "#ff6666", "cancel", true, ScanMode.BARCODE);
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt,
+                    ),
+                    label: CustomeText(
+                      text: "Product Code",
+                      textStyle: theme.textTheme.bodyLarge!
+                          .copyWith(color: Colors.white),
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton.icon(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blue.shade500),
+                        elevation: MaterialStateProperty.all(0.0)),
+                    onPressed: () async {
+                      await pickImage();
+                    },
+                    icon: const Icon(Icons.camera),
+                    label: CustomeText(
+                      text: "Upload image",
+                      textStyle: theme.textTheme.bodyLarge!
+                          .copyWith(color: Colors.white),
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: ElevatedButton.icon(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.orange.shade600),
+                          elevation: MaterialStateProperty.all(0.0)),
+                      onPressed: () async {},
+                      icon: const Icon(Icons.check),
+                      label: CustomeText(
+                        text: "Save",
+                        textStyle: theme.textTheme.bodyLarge!
+                            .copyWith(color: Colors.white),
+                      )),
+                )
               ],
             )),
       ),
